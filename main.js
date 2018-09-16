842a39235c6932f2161804ca65b82dfa469dce50
Austin Hou <austin@austinhou.com>
2018-09-16 01:02:52 -0700

    Calculator

---
+++ main.js
@@ -0,0 +1,29 @@
+function calculate(input) {
+  input = input.replace(/ /g,'');
+  let frames = input.split(",");
+
+  let groups = [];
+  frames.forEach(element => {
+    if (groups.length == 0) {
+      groups.push([element]);
+    }
+    else {
+      let len = groups.length;
+      if (groups[len-1][groups[len-1].length - 1] == element - 1) {
+        groups[len - 1].push(element);
+      }
+      else {
+        groups.push([element]);
+      }
+    }
+  })
+
+  console.log(groups);
+
+  let result = [];
+  groups.forEach(element => {
+    result.push(element[0] + "-" + element[element.length - 1]);
+  })
+
+  console.log(result);
+}
